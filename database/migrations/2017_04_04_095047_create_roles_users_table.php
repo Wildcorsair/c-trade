99dbdc98b637fc58ce6ed0366eb76f925f6ae40a
Roman Berezhnoi <berezhnoi.f5@gmail.com>
2017-04-04 13:01:10 +0300

    Changed and added new migrations for users table and roles table

---
+++ database/migrations/2017_04_04_095047_create_roles_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('role_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropForeign('roles_users_role_id_foreign');
+        Schema::dropForeign('roles_users_user_id_foreign');
+        Schema::dropIfExists('roles_users');
+    }
+}
